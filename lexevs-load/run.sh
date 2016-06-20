0770636843b79a5de11b28234c144a95002a1c51
Scott Bauer <bauer.scott@mayo.edu>
2016-06-20 17:01:48 -0500

    LEXEVS-1973.  Fixing some overwrites of this script causing addtl fails

--- lexevs-load/run.sh
+++ lexevs-load/run.sh
@@ -6,7 +6,7 @@ chmod 777 *
 
 ./LoadLgXML.sh -in ../test/resources/testData/German_Made_Parts.xml -a  -t "PRODUCTION"
 
-./LoadLgXML.sh -in ../test/resources/testData/Automobiles2.xml -a
+./LoadLgXML.sh -in ../test/resources/testData/Automobiles2.xml
 
 ./LoadLgXML.sh -in ../test/resources/testData/BoostedQuery.xml -a
 
@@ -22,14 +22,16 @@ chmod 777 *
 
 ./LoadLgXML.sh -in ../test/resources/testData/testExtension.xml -a
 
-./LoadLgXML.sh -in ../test/resources/testData/valueDomain/pickListTestData.xml -a
+./LoadLgXML.sh -in ../test/resources/testData/valueDomain/pickListTestData.xml
 
-./LoadLgXML.sh -in ../test/resources/testData/valueDomain/vdTestData.xml -a
+./LoadLgXML.sh -in ../test/resources/testData/valueDomain/vdTestData.xml
 
-./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGM"
+./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGM" -a
 
-./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGMWithlt250charName"
+./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGMWithlt250charName" -a
 
 ./LoadLgXML.sh -in ../test/resources/testData/valueDomain/VSD_OWL2Annotations.xml -a
+
+./LoadMetaBatch.sh -in ../test/resources/testData/SAMPLEMETA -a
 
 ./SupplementScheme.sh -r -parentUri "urn:oid:11.11.0.1" -parentVersion "1.0" -supplementUri "urn:oid:11.11.0.1.1-extension" -supplementVersion "1.0-extension"
