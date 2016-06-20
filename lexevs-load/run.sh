6cee6e6239ea49e566057e2f44634c64b8399eaf
Scott Bauer <bauer.scott@mayo.edu>
2016-06-20 12:32:52 -0500

    LEXEVS-1973.  Removing some apparent file contamination in this script

--- lexevs-load/run.sh
+++ lexevs-load/run.sh
@@ -12,24 +12,24 @@ chmod 777 *
 
 ./LoadOWL2.sh -in ../test/resources/testData/camera.owl -a
 
-./LoadOWL2.sh -in ../test/resources/testData/owl2/owl2-test-cases-Primitive-Annotated.owl -a
+./LoadOWL2.sh -in ../test/resources/testData/owl2/owl2-test-cases-Primitive-Annotated.owl -a
 
 ./LoadOBO.sh -in ../test/resources/testData/cell.obo -a
 
-./LoadOBO.sh -in ../test/resources/testData/fungal_anatomy.obo -a
+./LoadOBO.sh -in ../test/resources/testData/fungal_anatomy.obo -a
 
 ./LoadLgXML.sh -in ../test/resources/testData/testMapping.xml -a
 
 ./LoadLgXML.sh -in ../test/resources/testData/testExtension.xml -a
 
-./LoadLgXML.sh -in ../test/resources/testData/valueDomain/pickListTestData.xml -a
+./LoadLgXML.sh -in ../test/resources/testData/valueDomain/pickListTestData.xml -a
 
-./LoadLgXML.sh -in ../test/resources/testData/valueDomain/vdTestData.xml -a
+./LoadLgXML.sh -in ../test/resources/testData/valueDomain/vdTestData.xml -a
 
-./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGM"
+./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGM"
 
-./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGMWithlt250charName"
+./LoadResolvedValueSetDefinition.sh -u "SRITEST:AUTO:AllDomesticButGMWithlt250charName"
 
-./LoadLgXML.sh -in ../test/resources/testData/valueDomain/VSD_OWL2Annotations.xml -a
+./LoadLgXML.sh -in ../test/resources/testData/valueDomain/VSD_OWL2Annotations.xml -a
 
 ./SupplementScheme.sh -r -parentUri "urn:oid:11.11.0.1" -parentVersion "1.0" -supplementUri "urn:oid:11.11.0.1.1-extension" -supplementVersion "1.0-extension"
