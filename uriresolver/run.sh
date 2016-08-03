216f6fb6dbda7b69ebf1d505441017318b8b3cf8
Deepak Sharma <sharma.deepak2@mayo.edu>
2016-08-03 09:28:46 -0500

    feature/LEXEVS-2140:Introduced Java8, CentOS 7 and tomcat 7 with Java8 Changes

--- uriresolver/run.sh
+++ uriresolver/run.sh
@@ -1,3 +1,9 @@
-cp /artifacts/uriresolver.war /usr/local/tomcat/webapps/
+#!/bin/bash
+
+if [ ! -f ${CATALINA_HOME}/scripts/.tomcat_admin_created ]; then
+	${CATALINA_HOME}/scripts/create_admin_user.sh
+fi
+
+cp /artifacts/uriresolver.war /opt/tomcat/webapps/
 
 catalina.sh run
