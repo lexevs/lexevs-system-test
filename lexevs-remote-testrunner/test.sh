a30d96c96fd474560df2b602382f0e8f64999e8e
Endle, Cory M <Endle.Cory@mayo.edu>
2016-10-17 10:00:14 -0500

    LEXEVS-2402 Updated build scripts to include additional ANT_OPS and MAVEN_OPTS

--- lexevs-remote-testrunner/test.sh
+++ lexevs-remote-testrunner/test.sh
@@ -2,6 +2,8 @@ mv /Test.properties /lexevs-remote/LexEVSService/test/resources
 
 cd /lexevs-remote/LexEVSService/test
 
+export ANT_OPTS="-Dhttps.protocols=TLSv1.1,TLSv1.2 -Dforce.http.jre.executor=true -Xmx3072m -XX:MaxPermSize=752m"
+
 ant
 
 mkdir /results/remote-api/
