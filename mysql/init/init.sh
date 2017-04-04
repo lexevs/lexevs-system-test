939d8bd72a29a35245d373b559bb347389281da9
Scott Bauer <bauer.scott@mayo.edu>
2017-04-04 12:17:58 -0500

    LEXSYSTEST-4. Updating config to accmodate change in the MySQL 5.5 images.

--- mysql/init/init.sh
+++ mysql/init/init.sh
@@ -1,5 +1,5 @@
 echo "Initializing MySQL..."
-mysql -u root -proot --socket=/var/run/mysqld/mysqld.sock -e "CREATE DATABASE uriresolver" 
-mysql -u root -proot --socket=/var/run/mysqld/mysqld.sock -e "CREATE DATABASE lexevs"
-mysql -u root -proot --socket=/var/run/mysqld/mysqld.sock uriresolver < /sqlinit/uriresolver.sql
+mysql -u root -proot -e "CREATE DATABASE uriresolver" 
+mysql -u root -proot -e "CREATE DATABASE lexevs"
+mysql -u root -proot uriresolver < /sqlinit/uriresolver.sql
 echo "Initialization finished."
