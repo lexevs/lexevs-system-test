02ab6f16506872a08d70945b8dd26ca4dcdf4045
Cory Endle <cory.endle@nih.gov>
2017-05-17 08:57:34 -0500

    LEXEVSCTS2-62 Set a sleep timer and making results directory.

--- lexevs-cts2-testrunner/test.sh
+++ lexevs-cts2-testrunner/test.sh
@@ -1,3 +1,9 @@
+#wait 30 seconds... time to let the lexevs-cts2 container to start up.
+echo Sleeping 30 seconds to let the lexevs-cts2 container to start up.
+sleep 30
+
 jasmine-node --config baseURL http://lexevs-cts2:8080/lexevscts2/ cts2Calls_spec.js --junitreport
 
-cp -R reports/* results/cts2-rest/
+mkdir /results/lexevs-cts2-rest/
+cp -R reports/* /results/lexevs-cts2-rest/
+
