a74a2ce6383866613c6146766387feb734311e70
Bauer <bauerhs@nci-02033514-ml.nih.gov>
2017-08-24 08:45:15 -0500

    LEXSYSTEST-37.  Updating the load script.  Now down to 4 fails

--- lexevs-load/run.sh
+++ lexevs-load/run.sh
@@ -12,7 +12,7 @@ chmod 777 *
 
 ./LoadOWL2.sh -in ../test/resources/testData/camera.owl -a
 
-./LoadOWL2.sh -in ../test/resources/testData/owl2/owl2-special-cases-Defined-Annotated.owl -a
+./LoadOWL2.sh -in ../test/resources/testData/owl2/owl2-special-cases-Defined-Annotated.owl -a -t "PRODUCTION"
 
 ./LoadOBO.sh -in ../test/resources/testData/cell.obo -a
 
@@ -37,3 +37,5 @@ chmod 777 *
 ./LoadLgXML.sh -in ../test/resources/testData/valueDomain/VSD_OWL2Annotations.xml -a
 
 ./SupplementScheme.sh -r -parentUri "urn:oid:11.11.0.1" -parentVersion "1.0" -supplementUri "urn:oid:11.11.0.1.1-extension" -supplementVersion "1.0-extension"
+
+./SourceAssertedResolvedValueSetLoad.sh -cs "owl2lexevs" -v "0.1.5" -a "Concept_In_Subset" -t "true" -uri "http://evs.nci.nih.gov/valueset" -o "NCI" -s "Contributing_Source" 
