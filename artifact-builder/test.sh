39040f74c8f63a412d98c274a0a55ff0aa68fbc9
Cory Endle <cory.endle@nih.gov>
2018-01-11 12:04:13 -0600

    LEXEVS-3339 Updated script to skip cts2 and remote api tests

--- artifact-builder/test.sh
+++ artifact-builder/test.sh
@@ -2,8 +2,8 @@ BUILDDIR='/build'
 
 mkdir $BUILDDIR && cd $BUILDDIR
 
-export MAVEN_OPTS="-Dhttps.protocols=TLSv1.1,TLSv1.2 -Dforce.http.jre.executor=true -Xmx3072m -XX:MaxPermSize=752m"
-export ANT_OPTS="-Dhttps.protocols=TLSv1.1,TLSv1.2 -Dforce.http.jre.executor=true -Xmx3072m -XX:MaxPermSize=752m"
+export MAVEN_OPTS="-Dhttps.protocols=TLSv1.1,TLSv1.2 -Dforce.http.jre.executor=true -Xmx3072m"
+export ANT_OPTS="-Dhttps.protocols=TLSv1.1,TLSv1.2 -Dforce.http.jre.executor=true -Xmx3072m"
  
 # LexEVS
 git clone -b $LEXEVS_BRANCH $LEXEVS_REPO && \
@@ -11,30 +11,68 @@ git clone -b $LEXEVS_BRANCH $LEXEVS_REPO && \
     ant
 
 cp -f /lbconfig.props /build/lexevs/lbPackager/antbuild/resources/config
-
 cp /mysql-connector-java-5.1.37.jar /build/lexevs/lbPackager/antbuild/runtime/sqlDrivers
-
 cp -r /build/lexevs/lbPackager/antbuild/* /lexevs/
-
 cp -f /lbconfig.props /lexevs/resources/config/
-
 cp /mysql-connector-java-5.1.37.jar /lexevs/runtime/sqlDrivers
 
 # LexEVS Remote
-cd $BUILDDIR
-git clone -b $LEXEVS_REMOTE_BRANCH $LEXEVS_REMOTE_REPO && \
-    cd lexevs-remote/LexEVSService && \
-    cp /lexevs/runtime-components/lexbig.jar system/lib/ && \
-    cp /lexevs/test/lbTest.jar test/lib/ && \
-    ant && \
-    mv output/lexevsapi65/package/webapp/*.tomcat.war /artifacts/lexevsapi65.war && \
-    cp -r ../* /lexevs-remote/
+#cd $BUILDDIR
+#git clone -b $LEXEVS_REMOTE_BRANCH $LEXEVS_REMOTE_REPO && \
+#    cd lexevs-remote/LexEVSService && \
+#    cp /lexevs/runtime-components/lexbig.jar system/lib/ && \
+#    cp /lexevs/test/lbTest.jar test/lib/ && \
+#    ant && \
+#    mv output/lexevsapi65/package/webapp/*.tomcat.war /artifacts/lexevsapi65.war && \
+#    cp -r ../* /lexevs-remote/
+
+
+
+#
+# If -skipRemote, then we don't need to build the lexevs-remote
+#
+if [[ $TEST_OPTIONS ==  *"-skipRemote"* ]]; 
+then 
+	echo "** SKIP LEXEVS-REMOTE.  LEXEVS-REMOTE will not be built. "
+else
+	echo "** Building LEXEVS-REMOTE **"; 
+	cd $BUILDDIR
+	git clone -b $LEXEVS_REMOTE_BRANCH $LEXEVS_REMOTE_REPO && \
+	    cd lexevs-remote/LexEVSService && \
+    	cp /lexevs/runtime-components/lexbig.jar system/lib/ && \
+    	cp /lexevs/test/lbTest.jar test/lib/ && \
+    	ant && \
+    	mv output/lexevsapi65/package/webapp/*.tomcat.war /artifacts/lexevsapi65.war && \
+    	cp -r ../* /lexevs-remote/
+fi	    
+
 
 # URI Resolver
-cd $BUILDDIR
-git clone -b $URI_RESOLVER_BRANCH $URI_RESOLVER_REPO && \
-    cd URI_Resolver && \
-    mvn clean install && \   
-    mv target/*.war /artifacts/uriresolver.war && \
-    mkdir /results/uriresolver && \
-    cp -R target/surefire-reports/* /results/uriresolver/
+#cd $BUILDDIR
+#git clone -b $URI_RESOLVER_BRANCH $URI_RESOLVER_REPO && \
+#    cd URI_Resolver && \
+#    mvn clean install && \   
+#    mv target/*.war /artifacts/uriresolver.war && \
+#    mkdir /results/uriresolver && \
+#    cp -R target/surefire-reports/* /results/uriresolver/
+    
+
+#
+# If -skipCts2, then we don't need to build the URI Resolver
+#
+
+if [[ "$TEST_OPTIONS" == *"-skipCts2"* ]]; 
+then 
+	echo "** SKIP CTS2 TESTS.  URI RESOLVER will not be built."
+else
+	echo "** Building URI RESOLVER **"; 
+	cd $BUILDDIR
+	git clone -b $URI_RESOLVER_BRANCH $URI_RESOLVER_REPO && \
+	   	cd URI_Resolver && \
+    	mvn clean install && \   
+    	mv target/*.war /artifacts/uriresolver.war && \
+    	mkdir /results/uriresolver && \
+    	cp -R target/surefire-reports/* /results/uriresolver/
+fi
+    
+    
--- lexevs-testrunner/test.sh
+++ lexevs-testrunner/test.sh
@@ -5,8 +5,8 @@ cp -r /lexevs/* /test/
 
 cp -f /lbconfig.props /test/resources/config/
 
-cd /test/test && \
-   chmod 777 * && \
-   sh TestRunner.sh -x
+# cd /test/test && \
+#    chmod 777 * && \
+#    sh TestRunner.sh -x
 
 cp -R report.* /results/lexevs/
