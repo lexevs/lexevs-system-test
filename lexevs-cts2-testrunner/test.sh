99102a807ec0032214c1bc390a08766d0ce45b9c
Cory Endle <cory.endle@nih.gov>
2018-01-15 13:23:29 -0600

    LEXEVS-3339 Updated wait time for cts2 test runner

--- lexevs-cts2-testrunner/test.sh
+++ lexevs-cts2-testrunner/test.sh
@@ -1,6 +1,6 @@
-#wait 150 seconds... time to let the lexevs-cts2 container to start up.
-echo Sleeping 150 seconds to let the lexevs-cts2 container to start up.
-sleep 150
+#wait 200 seconds... time to let the lexevs-cts2 container to start up.
+echo Sleeping 200 seconds to let the lexevs-cts2 container to start up.
+sleep 200
 
 jasmine-node --config baseURL http://lexevs-cts2:8080/lexevscts2/ cts2Calls_spec.js --junitreport
 
