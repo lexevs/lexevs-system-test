4f9f8ac7510744ad4b75432bb9ae41e92fabbec0
Cory Endle <cory.endle@nih.gov>
2018-01-15 10:43:55 -0600

    LEXEVS-3339 Cleaning up code.

--- artifact-builder/test.sh
+++ artifact-builder/test.sh
@@ -18,17 +18,6 @@ cp -r /build/lexevs/lbPackager/antbuild/* /lexevs/
 cp -f /lbconfig.props /lexevs/resources/config/
 cp /mysql-connector-java-5.1.37.jar /lexevs/runtime/sqlDrivers
 
-# LexEVS Remote
-#cd $BUILDDIR
-#git clone -b $LEXEVS_REMOTE_BRANCH $LEXEVS_REMOTE_REPO && \
-#    cd lexevs-remote/LexEVSService && \
-#    cp /lexevs/runtime-components/lexbig.jar system/lib/ && \
-#    cp /lexevs/test/lbTest.jar test/lib/ && \
-#    ant && \
-#    mv output/lexevsapi65/package/webapp/*.tomcat.war /artifacts/lexevsapi65.war && \
-#    cp -r ../* /lexevs-remote/
-
-
 
 #
 # If -skipRemote, then we don't need to build the lexevs-remote
@@ -49,22 +38,12 @@ else
 fi	    
 
 
-# URI Resolver
-#cd $BUILDDIR
-#git clone -b $URI_RESOLVER_BRANCH $URI_RESOLVER_REPO && \
-#    cd URI_Resolver && \
-#    mvn clean install && \   
-#    mv target/*.war /artifacts/uriresolver.war && \
-#    mkdir /results/uriresolver && \
-#    cp -R target/surefire-reports/* /results/uriresolver/
-    
-
 #
 # If -skipCts2, then we don't need to build the URI Resolver
 #
 if [[ "$TEST_OPTIONS" == *"-skipCts2"* ]]; 
 then 
-	echo "** SKIP CTS2 TESTS.  URI RESOLVER will not be built."
+	echo "** SKIP CTS2.  URI RESOLVER will not be built."
 else
 	echo "** Building URI RESOLVER **"; 
 	cd $BUILDDIR
