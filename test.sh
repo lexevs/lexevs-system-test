04a0e8c68059b1e29543d27d56c3e3e5789009ad
Cory Endle <cory.endle@nih.gov>
2018-01-18 12:41:10 -0600

    LEXSYSTEST-54 Outputting contents of tomcat logs.

--- test.sh
+++ test.sh
@@ -190,19 +190,19 @@ echo
 echo ************** LEXEVS_REMOTE_CONTAINER
 echo
 
-#docker exec lexevs-remote cat /local/content/tomcat/container/logs/catalina.out
+docker exec lexevs-remote cat /local/content/tomcat/container/logs/catalina.out
 
 echo
 echo ************** LEXEVS_CTS2_CONTAINER
 echo
 
-#docker exec lexevs-cts2 cat /local/content/tomcat/container/logs/catalina.out
+docker exec lexevs-cts2 cat /local/content/tomcat/container/logs/catalina.out
 
 echo
 echo ************** URIRESOLVER_CONTAINER
 echo
 
-#docker exec uriresolver cat /local/content/tomcat/container/logs/catalina.out
+docker exec uriresolver cat /local/content/tomcat/container/logs/catalina.out
 
 
 echo
