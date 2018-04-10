db844fd9d792ab664c498544c143477ea38b7ec7
Cory Endle <cory.endle@nih.gov>
2018-04-10 15:53:40 -0500

    LEXSYSTEST-88 - Updated expected number of resolved value sets from 11 to 7.

--- lexevs-cts2-testrunner/cts2Calls_spec.js
+++ lexevs-cts2-testrunner/cts2Calls_spec.js
@@ -537,7 +537,7 @@ describe('CTS2 integration tests', function() {
 			.expect('header','content-type', 'application/json;charset=UTF-8')
 			.expect('json', 'ResolvedValueSetDirectory', {
            	 	complete: "COMPLETE",
-           	 	numEntries: 11
+           	 	numEntries: 7
          	})
          	.expect('json', 'ResolvedValueSetDirectory.entry.?', {
            	 	resolvedValueSetURI: "SRITEST:AUTO:AllDomesticButGM",
