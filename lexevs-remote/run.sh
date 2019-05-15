e7fb81280c8f12922fecf70df7347333fa20a721
Cory Endle <cory.endle@nih.gov>
2019-05-15 12:57:06 -0500

    LEXSYSTEST-143 Adjusted the run.sh script

--- lexevs-remote/run.sh
+++ lexevs-remote/run.sh
@@ -2,7 +2,6 @@ echo "START copying war to webapps"
 cp /lexevsapi65.war /local/content/tomcat/container/webapps/
 echo "DONE copying war to webapps"
 
-cd /usr/local/tomcat8.5/bin/
-./catalina.sh run
+/usr/local/tomcat8.5/bin/catalina.sh run
 
 echo "after staring tomcat"
