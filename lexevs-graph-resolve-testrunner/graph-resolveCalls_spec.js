7506c1bf05cd5eb7072d285f70065146dbbf0030
Cory Endle <cory.endle@nih.gov>
2019-12-03 09:55:48 -0600

    LEXSYSTEST-167 - Added tests calls for graph-resolve API

--- lexevs-graph-resolve-testrunner/graph-resolveCalls_spec.js
+++ lexevs-graph-resolve-testrunner/graph-resolveCalls_spec.js
@@ -11,7 +11,7 @@ var frisby = require('frisby');
 
 const Joi = frisby.Joi; // Frisby exposes Joi for convenience
 
-var timeoutSeconds = 1000 * 5;
+var timeoutSeconds = 1000 * 1;
 
 // Get the parameter passed in from the command line
 var baseURL = process.env['baseURL'];
@@ -28,10 +28,265 @@ describe('Graph-resolve integration tests', function() {
       .timeout(timeoutSeconds)
       .get(baseURL + 'databases')
 	    .expect('status', 200)
-//	    .expect('json', 'dataBases', {
-//           	 	0: 'owl2lexevs'
-//         	})
         .done(done);
 	}, timeoutSeconds);
 		
+//*********************************************************************
+// Test Query Outbound
+//*********************************************************************
+	it('Graph-resolve call: QueryOutbound', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/owl2lexevs/subClassOf/C61410')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C54443",
+           	namespace: "owl2lexevs"
+            })
+        .done(done);
+	}, timeoutSeconds);
+    
+//*********************************************************************
+// Test Query Outbound ConceptInSubset
+//*********************************************************************
+	it('Graph-resolve call: QueryOutboundConceptInSubset', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/owl2lexevs/Concept_In_Subset/C48323')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C54453",
+            code:"C117743",
+           	namespace: "owl2lexevs"
+            })
+        .done(done);
+	}, timeoutSeconds);
+    
+//*********************************************************************
+// Test QueryInbound ConceptInSubset
+//*********************************************************************
+	it('Graph-resolve call: QueryOutboundConceptInSubset', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/owl2lexevs/Concept_In_Subset/C48323')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C99998",
+            code:"C99999",
+            code:"C99988",
+            code:"C99989",
+           	namespace: "owl2lexevs"
+            })
+        .done(done);
+	}, timeoutSeconds);
+    
+//*********************************************************************
+// Test Query Inbound SubClassOf -- EXPECTING FAILURE
+//*********************************************************************
+	it('Graph-resolve call: QueryInBoundSubClassOf', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/owl2lexevs/subClassOf/C19448')
+	    .expect('status', 404)
+        .done(done);
+	}, timeoutSeconds);
+    
+//*********************************************************************
+// Test Query InBound PhasPrognosis -- EXPECTING FAILURE
+//*********************************************************************
+	it('Graph-resolve call: TestQueryInBoundHasPrognosis', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/owl2lexevs/patient_has_prognosis/CancerPatient')
+	    .expect('status', 404)
+        .done(done);
+	}, timeoutSeconds);
+      
+//*********************************************************************
+// Test Query OutBound HasPrognosis -- EXPECTING FAILURE
+//*********************************************************************
+	it('Graph-resolve call: TestQueryOutBoundPhasPrognosis', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/owl2lexevs/patient_has_prognosis/CancerPatient')
+	    .expect('status', 404)
+        .done(done);
+	}, timeoutSeconds);
+    
+    
+    
+//*********************************************************************
+// Test Query OutBound ConceptInSubset
+//*********************************************************************
+	it('Graph-resolve call: TestQueryOutBoundConceptInSubset', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/owl2lexevs/Concept_In_Subset/C48323')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C54453",
+            code:"C117743",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds);
+      
+//*********************************************************************
+// Test Query OutBound ConceptInSubset2
+//*********************************************************************
+	it('Graph-resolve call: TestQueryOutBoundConceptInSubset2', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/owl2lexevs/Concept_In_Subset/C99999')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C48323",
+            code:"C54453",
+            code:"C117743",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds);    
+    
+//*********************************************************************
+// Test Query InBound ConceptInSubset 
+//*********************************************************************
+	it('Graph-resolve call: TestQueryInBoundConceptInSubset - only 4', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/owl2lexevs/Concept_In_Subset/C48323')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C99998",
+            code:"C99999",
+            code:"C99988",
+            code:"C99989",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds); 
+    
+//*********************************************************************
+// Test Query OutBound GeneRelatedToDisease  -- EXPECTING FAILURE 
+//*********************************************************************
+	it('Graph-resolve call: TestQueryOutBoundGeneRelatedToDisease', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/owl2lexevs/gene_related_to_disease/NeoplasticDisease')
+	    .expect('status', 404)
+        .done(done);
+	}, timeoutSeconds); 
+    
+    
+//*********************************************************************
+// Test Query InBound ExactMatchCodeDepth1 
+//*********************************************************************
+	it('Graph-resolve call: TestQueryInBoundExactMatchCodeDepth1', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/1/owl2lexevs/Concept_In_Subset/C48323')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C99998",
+            code:"C99999",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds); 
+    
+    
+//*********************************************************************
+// Test Query InBound ExactMatchCodeDepth2
+//*********************************************************************
+	it('Graph-resolve call: TestQueryInBoundExactMatchCodeDepth2', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/2/owl2lexevs/Concept_In_Subset/C48323')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C99998",
+            code:"C99999",
+            code:"C99988",
+            code:"C99989",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds); 
+        
+	
+//*********************************************************************
+// Test Query OutBound ExactMatchCodeDepthPatient2 
+//*********************************************************************
+	it('Graph-resolve call: TestQueryOutBoundExactMatchCodeDepthPatient2', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/2/owl2lexevs/subClassOf/Patient')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"Person",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds); 
+    
+//*********************************************************************
+// Test Query InBound ExactMatchCodeDepthPatient1 
+//*********************************************************************
+	it('Graph-resolve call: TestQueryInBoundExactMatchCodeDepthPatient1', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/1/owl2lexevs/subClassOf/Patient')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"HappyPatientDrivingAround",
+            code:"HappyPatientWalkingAround",
+            code:"HealthyPatient",
+            code:"SickPatient",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds); 
+    
+//*********************************************************************
+// Test Query InBound ExactMatchCodeDepthPatient3
+//*********************************************************************
+	it('Graph-resolve call: TestQueryInBoundExactMatchCodeDepthPatient3', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getInbound/3/owl2lexevs/subClassOf/Patient')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"HappyPatientDrivingAround",
+            code:"HappyPatientWalkingAround",
+            code:"HealthyPatient",
+            code:"SickPatient",
+            code:"CancerPatient",
+            code:"MildlySickPatient",
+            code:"VerySickPatient",
+            code:"MildlySickCancerPatient",
+            code:"PatientWithCold",
+            code:"VerySickCancerPatient",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds); 
+    
+    
+//*********************************************************************
+// Test Query OutBound ExactMatchConceptInSubset2
+//*********************************************************************
+	it('Graph-resolve call: TestQueryOutBoundConceptInSubset2', function (done) {  
+	  frisby
+      .timeout(timeoutSeconds)
+      .get(baseURL + 'getOutbound/2/owl2lexevs/Concept_In_Subset/C48323')
+	    .expect('status', 200)
+        .expect('json', '?', {
+            code:"C54453",
+            code:"C117743",
+            namespace: "owl2lexevs"
+          })
+        .done(done);
+	}, timeoutSeconds); 
+    
+    
 });
