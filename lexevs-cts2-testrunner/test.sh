2a15c712b2de6300ad9b14f228ea3c2167413eee
Cory Endle <endle.cory@mayo.edu>
2022-02-08 09:59:26 -0600

    LEXSYSTEST-177 increase wait to 100 seconds for cts2 to start up

--- lexevs-cts2-testrunner/test.sh
+++ lexevs-cts2-testrunner/test.sh
@@ -1,6 +1,6 @@
 #wait 60 seconds... time to let the lexevs-cts2 container to start up.
-echo Sleeping 60 seconds to let the lexevs-cts2 container to start up.
-sleep 60
+echo Sleeping 100 seconds to let the lexevs-cts2 container to start up.
+sleep 100
 
 jasmine-node --config baseURL http://lexevs-cts2:8080/lexevscts2/ cts2Calls_spec.js --junitreport
 
